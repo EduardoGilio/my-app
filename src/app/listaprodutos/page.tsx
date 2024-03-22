8f820a902fe66dd30bfe1f0b937e11936017ed5f
EduardoGilio <134931339+EduardoGilio@users.noreply.github.com>
2024-03-22 11:37:02 -0300

    aula 22/03

--- src/app/listaprodutos/page.tsx
+++ src/app/listaprodutos/page.tsx
@@ -1,15 +1,32 @@
 import Link from "next/link";
 
+import produtos from "../../app/produtos"
+
 export default function listaprodutos () {
     return (
        <>
           <h1>Produtos</h1>
-          <ol>
+        {/* <ol>
            <Link href={"/listaprodutos/1"}><li>Produto 1 </li></Link>
            <Link href={"/listaprodutos/2"}><li>Produto 2 </li></Link>
            <Link href={"/listaprodutos/3"}><li>Produto 3 </li></Link>
           </ol>
+          </ol> */}
+          {/*produtos.map((prod) => <p>{prod.nome}</p>)}*/}
+          {/* () => {] */}
+
+         <ol>
+         {
+           produtos.map(
+             (prod) =>
+             <Link href={`/listaprodutos/${prod.id}`} key={prod.id}>
+               <li>{prod.nome}</li>
+             </Link>
+           )
+         }
+         </ol>
+         
           <Link href={"/"}>Home</Link>
-       </>
+         </>
     );
 }
